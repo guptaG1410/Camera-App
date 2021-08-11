a8626c2df3ae65a3178337aa8e8ce82a1f73816d
govind <64104987+guptaG1410@users.noreply.github.com>
2021-08-11 19:46:12 +0530

    Add files via upload

---
+++ dbScript.js
@@ -0,0 +1,13 @@
+let req = indexedDB.open("Camera", 1);
+
+req.addEventListener("success", function() {
+
+});
+
+req.addEventListener("upgradeneeded", function() {
+
+});
+
+req.addEventListener("error", function() {
+
+});
